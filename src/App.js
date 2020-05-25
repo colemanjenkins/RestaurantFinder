458a9305745738eb70515ed452b876ee5a04a0af
Alex Shen <shen.alexander@gmail.com>
2020-05-25 12:53:46 -0400

    Set up Google API w/ secret

--- src/App.js
+++ src/App.js
@@ -4,17 +4,32 @@ import TopPanel from './TopPanel';
 import List from "./List";
 import Map from "./Map";
 
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+
 export default class App extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      data: {},
+    }
   }
 
+  setData = (new_data) => {
+    this.setState({data: new_data})
+  } 
+
+  test = () => {
+    console.log("test")
+    // require('dotenv').load();
+    
+  }
   render(){
     return (
       <div className="App">
-        <TopPanel />
+        <TopPanel setData={this.setData}/>
         <List />
         <Map />
+        <button onClick={this.test}>TEST</button>
       </div>
     );
   }
--- src/TopPanel.js
+++ src/TopPanel.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react';
+import axios from 'axios'
+
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 export default class TopPanel extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    getData = () => {
+        const axios = require("axios");
+        
+        axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters', {
+            params: {
+                key: GOOGLE_API_KEY
+            }
+        })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          });  
+    }
     render(){
         return <div>
             TopPanel
+            <button onClick={this.getData}>Search</button>
         </div>
     }
 }

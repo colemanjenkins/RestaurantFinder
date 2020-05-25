d37861746e3167a016456acef2e6ac3dd3964913
Alex Shen <shen.alexander@gmail.com>
2020-05-25 11:08:22 -0400

    Updates

---
+++ precache-manifest.f37763511f4a9947da89ca00c7a5ecb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91491012b15552f43f3cb648a2bb6f4e",
+    "url": "/RestaurantApp/index.html"
+  },
+  {
+    "revision": "e26648abe9c155cd548c",
+    "url": "/RestaurantApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5bd7ee7622fb287a61ed",
+    "url": "/RestaurantApp/static/js/2.4785adf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RestaurantApp/static/js/2.4785adf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26648abe9c155cd548c",
+    "url": "/RestaurantApp/static/js/main.a05d3da8.chunk.js"
+  },
+  {
+    "revision": "0b34e7c1b94f7493cedf",
+    "url": "/RestaurantApp/static/js/runtime-main.38b6c4cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RestaurantApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
